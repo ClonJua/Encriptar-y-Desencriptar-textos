5a0aa40f19f6efe1091e4371c49d40da25cd7f28
Jose Navarro <104047183+ClonJua@users.noreply.github.com>
2024-08-25 22:07:25 -0500

    Aplicar restriccines

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ function encriptar() {
     var palabra = document.getElementById("palabra").value.toLowerCase(); //Pasar a minusculas
     var texto_encriptado = '';
     var conversion = {'e': 'enter', 'i': 'imes', 'a': 'ai', 'o': 'ober', 'u': 'ufat'}; //diccionario
-    
+    if (/[^a-z]/.test(palabra)) {
+        alert("No se permiten caracteres especiales");
+        return;
+    }else{
     for (var letra of palabra) {
         if (letra in conversion) {
             texto_encriptado += conversion[letra];
@@ -18,16 +21,23 @@ function encriptar() {
             texto_encriptado += letra; // Mantiene letras que no están en la conversión
         }
     }
+    }
     document.getElementById("resultado").value = texto_encriptado; //textarea
 }
 //uso de funcion replace
 function desencriptar() {
-    var palabra_encriptada = document.getElementById("resultado").value;
+    var palabra_encriptada = document.getElementById("resultado").value.toLowerCase;
+    if (/[^a-z]/.test(palabra)) {
+        alert("No se permiten caracteres especiales");
+        return;
+    }else{
     var texto_desencriptado = palabra_encriptada.replace(/enter/g, 'e')
                                                .replace(/imes/g, 'i')
                                                .replace(/ai/g, 'a')
                                                .replace(/ober/g, 'o')
                                                .replace(/ufat/g, 'u');
+    }
+     
     document.getElementById("resultado").value = texto_desencriptado;
 }
 
