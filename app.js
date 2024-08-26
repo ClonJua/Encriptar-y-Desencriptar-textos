c1e395486655e8614ed529e8f5a01574fdd9ebad
Jose Navarro <104047183+ClonJua@users.noreply.github.com>
2024-08-25 21:54:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+asignarTexto('h1', 'WEB PARA ENCRIPTAR Y DESENCRIPTAR PALABRAS');
+
+function asignarTexto(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+//uso de diccionario
+function encriptar() {
+    var palabra = document.getElementById("palabra").value.toLowerCase(); //Pasar a minusculas
+    var texto_encriptado = '';
+    var conversion = {'e': 'enter', 'i': 'imes', 'a': 'ai', 'o': 'ober', 'u': 'ufat'}; //diccionario
+    
+    for (var letra of palabra) {
+        if (letra in conversion) {
+            texto_encriptado += conversion[letra];
+        } else {
+            texto_encriptado += letra; // Mantiene letras que no están en la conversión
+        }
+    }
+    document.getElementById("resultado").value = texto_encriptado; //textarea
+}
+//uso de funcion replace
+function desencriptar() {
+    var palabra_encriptada = document.getElementById("resultado").value;
+    var texto_desencriptado = palabra_encriptada.replace(/enter/g, 'e')
+                                               .replace(/imes/g, 'i')
+                                               .replace(/ai/g, 'a')
+                                               .replace(/ober/g, 'o')
+                                               .replace(/ufat/g, 'u');
+    document.getElementById("resultado").value = texto_desencriptado;
+}
+
+function copiar(){
+    var textoCopiar = document.getElementById("resultado").value;
+    
+    navigator.clipboard.writeText(textoCopiar)
+  .then(() => {
+    console.log('Texto copiado al portapapeles')
+  })
+  .catch(err => {
+    console.error('Error al copiar al portapapeles:', err)
+  })
+}
+
